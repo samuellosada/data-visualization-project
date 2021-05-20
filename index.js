9305f48ab40dfc109cce902b394930e47edde990
samuellosada <samthesam26@gmail.com>
2021-05-20 16:32:19 +1000

    Added value of materials to default treemap, formatted them with commas.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 function main(){
-
     /*
     STEPS: 
     - create SVG area to show visualization.
     - get the data to a working state within the context of the program.
     - put the data into a treemap chart.
+    - Add onclick, mouseover events
+    - add second child layer of the dataset in its own window. 
     - add design elements.
     - add animations.
     */
@@ -15,7 +16,7 @@ function main(){
     let height = 800;
 
     let svg = d3.select(document.getElementById('visualization'))
-        .append('svg')
+        .append('svg') 
         .attr('width', width)
         .attr('height', height)
         .append('g');
@@ -32,8 +33,6 @@ function main(){
             wasteCategories2016.wasteCategories.push({name, totalAmount});
         }
 
-        console.log(wasteCategories2016)
-
         //Create new Layout to make a treemap. Gives each child a given x, y and other properties we use to make treemap visualizations
         let treemapLayout = d3.treemap()
            .size([700, 700])
@@ -42,20 +41,16 @@ function main(){
 
         //apparently allows you to select d3 layouts for hierarchical data. 
         let rootNode = d3.hierarchy(wasteCategories2016, (d)=> {return d.wasteCategories}); //Need to be able to let it know under what name all the children we need are.?? MAYBE THE SOLUTION???
-        
 
         //provides value data to the value properties added.
         rootNode.sum((d) => {
             return d.totalAmount;
         });
 
-        rootNode.sort((a, b) => b.value - a.value);
-
-        //rootNode.sort()  ADD Sort to make the other smaller squares more visible? 
+        rootNode.sort((a, b) => b.value - a.value); // makes the squares more visible.
 
         //assign the treemap layout to the hierarchichal data
         treemapLayout(rootNode);
-        console.log(rootNode)
         
         svg
             .selectAll("rect")
@@ -64,14 +59,13 @@ function main(){
             .append("rect")
                 .attr('x', (d) => {return d.x0})
                 .attr('y', (d) => {return d.y0})
-                .attr('width', (d) => { 
-                    console.log(d)
+                .attr('width', (d) => {
                     return d.x1 - d.x0})
                 .attr('height', (d) => {return d.y1 - d.y0})
                 .style('fill', "gray");
 
         svg 
-            .selectAll("text")
+            .selectAll("g")
             .data(rootNode.leaves())
             .enter()
             .append("text")
@@ -79,12 +73,28 @@ function main(){
                 .attr("y", (d) => {return d.y0 + 20})
                 .text((d) => {return d.data.name}) //data is used to access the leaf node properties.
                 .attr("font-size", "15px")
-                .attr("fill", "white")
+                .attr("fill", "white");
 
+        svg 
+            .selectAll("g")
+            .data(rootNode.leaves())
+            .enter()
+            .append("text")
+                .attr("x", (d) => {return d.x0 + 5})
+                .attr("y", (d) => {return d.y0 + 50})
+                .text((d) => {return d.data.totalAmount.toLocaleString('en-US')}) 
+                .attr("font-size", "15px")
+                .attr("fill", "white");
+
+        
 
     }, (err) => {
         alert(err)
     });
+}
+
+function handleWasteCategoryClick(d, i){
+    console.log(d);
 }
 
 window.onload = () => {
